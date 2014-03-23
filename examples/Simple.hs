5a8f6455f10d75433b43716e783b5b422e6658d7
alexbiehl <alex.biehl@gmail.com>
2014-03-23 11:20:23 +0100

    Example usage of session id

--- examples/Simple.hs
+++ examples/Simple.hs
@@ -7,14 +7,22 @@ import Pipes.ByteString (stdout)
 
 settings :: Settings
 settings = defaultSettings {
-    settingsTLS = Just $ (tlsSettings "server.crt" "server.key") {
-      security = DemandStartTLS
-    },
     settingsOnOpen = \sid -> do
       putStrLn $ show sid ++ " session opened"
     ,
     settingsOnClose = \sid -> do
       putStrLn $ show sid ++ " session closed"
+    ,
+    settingsOnMailFrom = \sid addr -> do
+      putStrLn $ show sid ++ " mail from " ++ show addr
+      return Accepted
+    ,
+    settingsOnRecipient = \sid addr -> do
+      putStrLn $ show sid ++ " rcpt to " ++ show addr
+      return Accepted
+    ,
+    settingsOnStartTLS = \sid -> do
+      putStrLn $ show sid ++ " starttls"
   }
 
 main :: IO ()
@@ -22,6 +30,6 @@ main = do
     runSettings settings app
   where
     app (Mail sid _ _ body) = do
-      putStrLn $ show sid ++ " receiving mail"
+      putStrLn $ show sid ++ " data"
       runEffect $ body >-> stdout
       return Accepted
